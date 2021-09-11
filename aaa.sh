4fada31b42ead3619331992356578892bf733c55
unknown <akshayHVshinde04@gmail.com>
2021-09-11 20:24:16 +0530

    first commit

---
+++ aaa.sh
@@ -0,0 +1,20 @@
+echo "Enter Degree Celsius temp (*C)"
+
+read temp
+
+  function degC()
+{
+
+    if (($1>=32 && $1<=300))
+    then
+
+    
+echo "Valid $degC "
+
+   else
+       echo "enter another value $degC"
+fi
+}
+   degC $temp
+
+
